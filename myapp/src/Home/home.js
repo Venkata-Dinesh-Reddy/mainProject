be4eee19237fb1c95c3b35ba7ad59e97a903493d
Prem Chand <thulluru.premchand@gmail.com>
2023-01-04 00:31:24 +0530

    Changing BG color in home javascript file

--- myapp/src/Home/home.js
+++ myapp/src/Home/home.js
@@ -5,7 +5,7 @@ const HomePage=() =>{
     const renderHomePage = () => {
         return(
             <div className="w-96 h-96">
-                <p className="font-bold bg-current p-6">Hi i am in the Home Page</p>
+                <p className="font-bold bg-slate-300 p-6">Hi i am in the Home Page</p>
             </div>
         )
     }
